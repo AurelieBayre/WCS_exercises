56d0a8ab0ba4cb2e2913f87014448cfcd1bb2c36
Aurelie Bayre <a.bayre@gmail.com>
2018-03-12 15:31:05 +0100

    FIX: app now can determine whether input is NaN, and whether it's bigger than 99

---
+++ intro-nodejs/app.js
@@ -0,0 +1,26 @@
+const actualDate = new Date()
+const actualYear = actualDate.getFullYear()
+
+const sentence = (num) => { 
+    const dateOfBirth = actualYear - num 
+    return console.log(`Mmmmhh... let me see my cristal ball...
+    \b You were born around the year ${dateOfBirth}`)
+}
+
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+
+console.log('How old are you?')
+process.stdin.on('data', (input) => {
+//input is a string!
+//return console.log(parseInt(input), typeof parseInt(input)) //NaN, "number" 
+    if (isNaN(input)) {
+        return console.log("sorry, this isn't a number. ")
+    }
+    else if (input > 99){
+        return console.log("sorry, this number is too big.")
+    }
+    return sentence(input)
+  process.exit()
+})
+
